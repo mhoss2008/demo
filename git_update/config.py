e6748d527bd245786a0d10eb6ae966b0e224b19d
Matthew Hoss <matthew.hoss@san-00000239.local>
2021-08-25 11:33:16 -0700

    updated path

---
+++ git_update/config.py
@@ -0,0 +1,15 @@
+"""config file"""
+from datetime import datetime
+import git
+MODE = "debug"
+
+
+def git_path():
+  git_path = "/home/matt/Git/demo/git_update"
+  return (git_path)
+
+def main():
+    return()
+
+if __name__ == "__main__":
+    main()
